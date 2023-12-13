41a3f7ab5b33517ed11c4042c56830bd813ff75e
Shubhajit612 <shubhajit612@gmail.com>
2023-12-13 22:25:20 +0530

    sequalize

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
-const db = require('./util/database');
+const sequalize = require('./util/database');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -27,4 +27,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequalize.sync().then(result =>{
+    console.log(result);
+    app.listen(3000);
+})
+.catch(err=>{
+    console.log(err);
+});
+
--- util/database.js
+++ util/database.js
@@ -1,10 +1,5 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'baba123',
-    password:'Toton@612'
-});
+const sequelize = new Sequelize('baba123','root','Toton@612',{dialect: 'mysql',host:'localhost'});
 
-module.exports = pool.promise();
+module.exports = sequelize;
